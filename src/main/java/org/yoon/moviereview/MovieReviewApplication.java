c6a4081296aebdf879f7625b7f9da878cca4ce0b
newcodingtest <pulpul8282@naver.com>
2021-12-20 10:58:13 +0900

    Initial commit

---
+++ src/main/java/org/yoon/moviereview/MovieReviewApplication.java
@@ -0,0 +1,13 @@
+package org.yoon.moviereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yoon/moviereview/MovieReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yoon.moviereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
